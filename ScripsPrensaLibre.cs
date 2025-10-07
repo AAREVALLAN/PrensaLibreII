2016abc86a3f942ee48f83e443ba0556390bf45c
Allan <163067794+AAREVALLAN@users.noreply.github.com>
2025-10-06 21:24:42 -0600

    Add files via upload

---
+++ ScripsPrensaLibre.cs
@@ -0,0 +1,13 @@
+﻿namespace NoticiasApp.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<Noticia> Noticias { get; set; }
+        public ICollection<Comentario> Comentarios { get; set; }
+    }
+}
